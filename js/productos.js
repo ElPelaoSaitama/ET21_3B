a656e7f4ae04ad230e8390024d87711a32431816
Pelao Rico <gustavocartes05@gmail.com>
2022-05-28 13:36:47 -0400

    s

--- js/productos.js
+++ js/productos.js
@@ -1,67 +1,42 @@
 const productos =[
     {
-      precio: 500,
-      id: 1,
-      title: "Café",
-      thumbnailUrl: "https://www.maceteros.es/media/catalog/product/cache/4/image/650x/040ec09b1e35df139433887a97daa66f/c/u/cubo-negro_1.jpg",
-      "desc": "blablabla desc"
+      "id": 1,
+      "title": "Macetas",
+      "thumbnailUrl": "../img/cat_macetas.jpg",
+      "desc": "Maceteros muy bonitos ;)",
+      "link": "sub-pages/macetas.html"
     },
     {
-      "precio": 300,
       "id": 2,
-      "title": "Pizza",
-      "thumbnailUrl": "https://picsum.photos/id/10/600",
-      "desc": "blablabla desc"
+      "title": "Flores",
+      "thumbnailUrl": "../img/cat_flores.jpg",
+      "desc": "Bonitas Flores ;)",
+      "link": "sub-pages/flores.html"
     },
     {
-      "precio": 100,
       "id": 3,
-      "title": "Agua",
-      "thumbnailUrl": "https://picsum.photos/id/20/600",
-      "desc": "blablabla desc"
-    },
-    {
-      "precio": 50,
-      "id": 4,
-      "title": "Sandía",
-      "thumbnailUrl": "https://picsum.photos/id/30/600",
-      "desc": "blablabla desc"
-    },
-    {
-      "precio": 10,
-      "id": 5,
-      "title": "Mango",
-      "thumbnailUrl": "https://picsum.photos/id/40/600",
-      "desc": "blablabla desc"
-    },
-    {
-      "precio": 150,
-      "id": 6,
-      "title": "Chela",
-      "thumbnailUrl": "https://picsum.photos/id/50/600",
-      "desc": "blablabla desc"
+      "title": "Plantas",
+      "thumbnailUrl": "../img/cat_plantas.jpg",
+      "desc": "Bonitas Plantas ;)",
+      "link": "sub-pages/plantas.html"
     }
   ]
-
-
-
 var texto_iterable=""
 
 productos.forEach((element) => {
-
     var tarjetita_html = `
-
 <div class="col-4">
     <div class="row">             
         <div class="container-card">
-            <div class ="card">
+        <a href="${element.link}" stretched-link">
+        <div class ="card">
                 <img src="${element.thumbnailUrl} " class "card-img-top" alt="foto">
                 <h3 class ="card-litle"> ${element.title} </h3>
-                <p class ="card-text "> ${element.desc} </p>
-                <a href="" class="btn btn-primary stretched-link">precio ${element.precio} </a>
+                <p class ="card-text "> ${element.desc}</p></a>   
             </div>
         </div>
     </div>
+    <br>
 </div>`
     texto_iterable += tarjetita_html
     $("#lol").html( texto_iterable )

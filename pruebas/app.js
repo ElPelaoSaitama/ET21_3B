bad4e9b3d27d0439beef331bc6dce106d0ddab61
jordan bolbaran <bikbokzz.78@gmail.com>
2022-05-29 13:11:59 -0400

    .

--- pruebas/app.js
+++ pruebas/app.js
@@ -134,17 +134,3 @@ const btnAumentarDisminuir = e => {
     }
     e.stopPropagation()
 }
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', e => {
-    fetchData()
-    if (localStorage.getItem('carrito')) {
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        pintarCarrito()
-    }
-});
-
